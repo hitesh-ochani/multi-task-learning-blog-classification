871dd06abb08f34f3c74aa48fde13a444afd50d9
Hitesh Ochani <hitesh.k.ochani@gmail.com>
2018-12-16 20:38:54 -0500

    to run on prince

---
+++ prince/load_data.py
@@ -0,0 +1,110 @@
+# _*_ coding: utf-8 _*_
+
+import os
+import sys
+import torch
+from torch.nn import functional as F
+import numpy as np
+from torchtext import data
+from torchtext import datasets
+from torchtext.vocab import Vectors, GloVe
+
+import pickle
+import re
+import spacy
+NLP = spacy.load('en')
+MAX_CHARS = 20000
+
+torch.manual_seed(999)
+import random
+random.seed(100)
+
+import csv
+
+csv.field_size_limit(sys.maxsize)
+
+def tokenizer(comment):
+    comment = re.sub(
+        r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ",
+        str(comment))
+    comment = re.sub(r"[ ]+", " ", comment)
+    comment = re.sub(r"\!+", "!", comment)
+    comment = re.sub(r"\,+", ",", comment)
+    comment = re.sub(r"\?+", "?", comment)
+    if (len(comment) > MAX_CHARS):
+        comment = comment[:MAX_CHARS]
+    return [
+        x.text for x in NLP.tokenizer(comment) if x.text != " "]
+
+def load_dataset(test_sen=None):
+
+    """
+    tokenizer : Breaks sentences into a list of words. If sequential=False, no tokenization is applied
+    Field : A class that stores information about the way of preprocessing
+    fix_length : An important property of TorchText is that we can let the input to be variable length, and TorchText will
+                 dynamically pad each sequence to the longest sequence in that "batch". But here we are using fi_length which
+                 will pad each sequence to have a fix length of 200.
+
+    build_vocab : It will first make a vocabulary or dictionary mapping all the unique words present in the train_data to an
+                  idx and then after it will use GloVe word embedding to map the index to the corresponding word embedding.
+
+    vocab.vectors : This returns a torch tensor of shape (vocab_size x embedding_dim) containing the pre-trained word embeddings.
+    BucketIterator : Defines an iterator that batches examples of similar lengths together to minimize the amount of padding needed.
+
+    """
+
+
+#    TEXT = data.Field(sequential=True, tokenize=tokenize, lower=True, include_lengths=True, batch_first=True, fix_length=200)
+    LABEL = data.LabelField(tensor_type=torch.cuda.FloatTensor) #torch.cuda.FloatTensor
+    AGE_BINS = data.LabelField(tensor_type=torch.cuda.FloatTensor) #torch.cuda.FloatTensor
+    INDEX =  data.Field(tensor_type=torch.cuda.LongTensor) #torch.cuda.LongTensor
+
+    TEXT = data.Field(
+        sequential=True,
+        fix_length=200,
+        tokenize=tokenizer,
+        pad_first=True,
+        tensor_type=torch.cuda.LongTensor,
+        lower=True,
+        batch_first=True)
+    #***** this was earlier tensor_type=torch.cuda.LongTensor,
+
+
+
+    train_data, test_data = data.TabularDataset.splits(
+    path='.', format='csv', skip_header=True,
+    train='blogs_training.csv', validation='blogs_testing.csv',
+    fields=[
+        ('index', None),
+        ('text', TEXT),
+        ('fileIndex', None),
+        ('label', LABEL),
+        ('age', None),
+        ('industry', None),
+        ('hscope', None),
+        ('age_bins',AGE_BINS)
+    ])
+
+
+#    train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
+    TEXT.build_vocab(train_data, vectors=GloVe(name='twitter.27B', dim=100))
+    LABEL.build_vocab(train_data)
+    AGE_BINS.build_vocab(train_data)
+
+    pickle.dump(TEXT, open("TEXT.pickle","wb"))
+
+    word_embeddings = TEXT.vocab.vectors
+    print ("Length of Text Vocabulary: " + str(len(TEXT.vocab)))
+    print ("Vector size of Text Vocabulary: ", TEXT.vocab.vectors.size())
+    print ("Label Length: " + str(len(LABEL.vocab)))
+    print ("AGE BINS Label Length: " + str(len(AGE_BINS.vocab)))
+
+    train_data, valid_data = train_data.split(random_state=random.getstate()) # Further splitting of training_data to create new training_data & validation_data
+    train_iter, valid_iter, test_iter = data.BucketIterator.splits((train_data, valid_data, test_data), batch_size=32, sort_key=lambda x: len(x.text), repeat=False, shuffle=True)
+
+    '''Alternatively we can also use the default configurations'''
+    # train_iter, test_iter = datasets.IMDB.iters(batch_size=32)
+
+    vocab_size = len(TEXT.vocab)
+
+    return TEXT, vocab_size, word_embeddings, train_iter, valid_iter, test_iter
